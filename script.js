5822bd8a22e4eb1b67aa5a327f7f7807c99db1fb
unknown <ivanlorenzoruiz@alumnos.tuniversformacion.es>
2023-09-20 09:40:26 +0200

    Añadida la funcionalidad de cambiar el color del fondo

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-const btn = document.querySelector('button');
+
+
+/* const bot = document.querySelector('button');
 const body = document.querySelector('body');
 const colores=['green', 'red', 'white', 'pink', 'black'];
 
-btn.addEventListener('click', fondo);
+bot.addEventListener('click', fondo);
 
 function fondo() {
     const indice=parseInt(Math.random() * colores.length);
     body.style.backgroundColor - colores[indice];
 
 }
+*/
+
+
+const generateRandomColor = () => {
+    const color = '#' + Math.floor(Math.random() * 16777215).toString(16)
+    console.log("Color hexadecimal generado: " + color)
+    return color
+}
+function generateRandomColor2() {
+    const color= '#'+ Math.floor (Math.random()* 16777215).toString(16)
+    console.log("Color hexadecimal generado: "+ color)
+    return color
+}
+// Función para cambiar el color de fondo del body //
+const changeBackgroundColor = () => {
+    const newColor = generateRandomColor();
+    document.body.style.backgroundColor = newColor;
+
+}
+// Botón //
+const changeColorBtn = document.getElementById("cambiar_color");
